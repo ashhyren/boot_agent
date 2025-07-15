e6d006791a168680abe95f172b30bb7c2152eead
ashhyren <ashhyren@hotmail.com>
2025-07-15 16:30:44 -0700

    1:1-4

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    print("Hello from boot-agent!")
+
+    if len(sys.argv) == 1:
+        print("ERROR: No prompt given")
+        sys.exit(1)
+    else:
+        question = " ".join(sys.argv[1:])
+
+    print(f"\nPROMPT: \n{question}")
+    print("\nRESPONSE:")
+
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=question
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
