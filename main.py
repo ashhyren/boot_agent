bcf0cdc262d1417daa4ed0b100245bef597a7e12
ashhyren <ashhyren@hotmail.com>
2025-07-15 17:02:21 -0700

    1:5-6

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
@@ -13,22 +14,32 @@ def main():
 
     print("Hello from boot-agent!")
 
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append[arg]
+
     if len(sys.argv) == 1:
         print("ERROR: No prompt given")
         sys.exit(1)
     else:
-        question = " ".join(sys.argv[1:])
+        question = " ".join(args)
 
-    print(f"\nPROMPT: \n{question}")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=question)]),
+    ]
+
+    if sys.argv[-1] == "--verbose":
+        print(f"\nUser prompt: \n{question}")
     print("\nRESPONSE:")
 
-
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=question
+        model='gemini-2.0-flash-001', contents=messages
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if sys.argv[-1] == "--verbose":
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
